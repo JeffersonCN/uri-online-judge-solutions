8f3ddb904319b980c55fc4f297c7287e8c3fd55c
Jefferson Nascimento <jefferson.nascimento3@fatec.sp.gov.br>
2016-04-16 23:40:43 -0300

    Add solution for the problem 1006 - Average 2

---
+++ Problems/1. Beginner/1006 - Average 2/Main.java
@@ -0,0 +1,15 @@
+// PROBLEM URL: https://www.urionlinejudge.com.br/judge/en/problems/view/1005
+
+import java.util.Scanner;
+import java.util.Locale;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in).useLocale(Locale.US);
+        double a = scan.nextDouble();
+        double b = scan.nextDouble();
+        double c = scan.nextDouble();
+        double media = ((a*2) + (b*3) + (c*5))/10;
+        System.out.println(String.format("MEDIA = %.1f", media));
+    }
+}
