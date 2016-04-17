cfaa2f6804fa5887e827114637380b03bb334703
Jefferson Nascimento <jefferson.nascimento3@fatec.sp.gov.br>
2016-04-16 22:22:40 -0300

    Add solutions for beginner problems 1001 and 1002

---
+++ Problems/1. Beginner/1001/Main.java
@@ -0,0 +1,12 @@
+// PROBLEM URL: https://www.urionlinejudge.com.br/judge/pt/problems/view/1001
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int a = scan.nextInt();
+        int b = scan.nextInt();
+        System.out.println(String.format("X = %d", a+b));
+    }
+}
---
+++ Problems/1. Beginner/1002/Main.java
@@ -0,0 +1,13 @@
+// PROBLEM URL: https://www.urionlinejudge.com.br/judge/pt/problems/view/1002
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        final double pi = 3.14159;
+        Scanner scan = new Scanner(System.in);
+        double radius = Double.parseDouble(scan.next());
+        double area = pi * radius * radius;
+        System.out.println(String.format("A=%.4f", area));
+    }
+}
