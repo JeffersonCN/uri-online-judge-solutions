ae117d0605fdd313a9f0dea5a9a94e121c987677
Jefferson Nascimento <jefferson.nascimento3@fatec.sp.gov.br>
2016-04-16 23:24:33 -0300

    Add java.util.Locale to read the entries properly

--- Problems/1. Beginner/1002 - Area of a Circle/Main.java
+++ Problems/1. Beginner/1002 - Area of a Circle/Main.java
@@ -1,12 +1,13 @@
 // PROBLEM URL: https://www.urionlinejudge.com.br/judge/en/problems/view/1002
 
 import java.util.Scanner;
+import java.util.Locale;
 
 public class Main {
     public static void main(String[] args) {
         final double pi = 3.14159;
-        Scanner scan = new Scanner(System.in);
-        double radius = Double.parseDouble(scan.next());
+        Scanner scan = new Scanner(System.in).useLocale(Locale.US);
+        double radius = scan.nextDouble();
         double area = pi * radius * radius;
         System.out.println(String.format("A=%.4f", area));
     }
