1c0337b79aeee67018442168e3ddce5b3c48042a
Jefferson Nascimento <jefferson.nascimento3@fatec.sp.gov.br>
2016-04-17 01:29:38 -0300

    Add solution for problem 1013

---
+++ Problems/1. Beginner/1013 - The Greatest/Main.java
@@ -0,0 +1,21 @@
+// PROBLEM URL: https://www.urionlinejudge.com.br/judge/en/problems/view/1013
+
+import java.util.Scanner;
+import java.util.Locale;
+import java.lang.Math;
+
+public class Main {
+    public static void main(String[] args) {
+        final double pi = 3.14159;
+        Scanner scan = new Scanner(System.in).useLocale(Locale.US);
+        
+        int a = scan.nextInt();
+        int b = scan.nextInt();
+        int c = scan.nextInt();
+        
+        int maiorAB = (a + b + Math.abs(a - b)) / 2;
+        int maiorABC = (maiorAB + c + Math.abs(maiorAB - c)) / 2;
+        
+        System.out.println(String.format("%d eh o maior", maiorABC));
+    }
+}
